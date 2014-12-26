c95d393ab2496ad9a47ea7f52e1668997b218418
Jeff Quinn <jeff@nunahealth.com>
2014-12-26 11:48:07 -0800

    factor out bower files

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-s3cmd del --recursive s3://math-teaching-tools/decimal-number-line/
-s3cmd put --recursive app s3://math-teaching-tools/decimal-number-line/
+s3cmd del --recursive s3://math-teaching-tools/app/
+s3cmd put --recursive app s3://math-teaching-tools/
 s3cmd put index.html s3://math-teaching-tools/index.html
